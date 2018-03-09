53c88e674bb06fa92f0499d92252f39c11cbc84c
Arvin <wangchenglonge@qq.com>
2018-03-09 11:51:38 +0800

    修改为 linux 代码
    std::cout 在 linux 中报错

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	cout << "Hello World!" << endl;
+	int first = 1234567;
+	char second = first;
+	long third = second;
+
+	printf("Hello, world\n");
+	printf("Welcom\n");
+	printf("automatic build\n");
 	
 	return 0;
 }
