2dc1e13030fac9617d92e743c9df1f0674252b3c
Arvin <wangchenglonge@qq.com>
2018-04-17 19:12:09 +0800

    add cpp

---
+++ cp/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fun1()
+{
+	int a = 1234567890;
+	char c = a;
+	
+	printf("%s\n",a);
+	printf("%d\n",a);
+}
+
+int main(int argc, char *argv[])
+{
+	fun1();
+
+	int first = 1234567;
+	char second = first;
+	long third = second;
+
+	printf("Hello, world\n");
+	printf("Welcom\n");
+	printf("automatic build\n");
+	
+	return 0;
+}
