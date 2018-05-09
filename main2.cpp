d68f2918431134c8dfdec2ac764822d9415a69d4
Arvin <wangchenglonge@qq.com>
2018-05-09 19:01:50 +0800

    add cpp

---
+++ main2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int fun3(){
+	return "123";
+}
+
+int main(int argc, char *argv[])
+{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+	printf("Hello, world\n");
+	
+	return 0;
+}
