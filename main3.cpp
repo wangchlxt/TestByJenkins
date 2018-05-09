d955f58523fb0a6492f9b68dfd5c5d3c77cd2c2e
Arvin <wangchenglonge@qq.com>
2018-05-09 20:20:18 +0800

    up cpp

--- main3.cpp
+++ main3.cpp
@@ -10,9 +10,16 @@ int int abc()
 	
 }
 
+int a;
+string b;
+long c;
+char d;
+int* e;
+
 int main(int argc, char *argv[])
 {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 	printf("Hello, world\n");
-	
+	printf(e);
+
 	return 0;
 }
